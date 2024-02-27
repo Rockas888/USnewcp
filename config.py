b4bcdf9ca6c12d5cb01617c38bcad17a365d03d2
Rockas888 <105117889+Rockas888@users.noreply.github.com>
2024-02-27 15:11:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6152562853:AAGimPmtvHjqcE8em9iDMH-QAjkM8133P0c")
-    API_ID = int(os.environ["API_ID", 22779671]
-    API_HASH = os.environ["API_HASH", "22779671"]
-    AUTH_USERS = "1713924419"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7186291829:AAGHlSx7Y-iyYP2qHVSBe9iSt003gWsuqkw
+    API_ID = int(os.environ["API_ID", 27789935]
+    API_HASH = os.environ["API_HASH", "1a1bb0a810826c11f9303281caf77b87"]
+    AUTH_USERS = "6585878012"""
 
